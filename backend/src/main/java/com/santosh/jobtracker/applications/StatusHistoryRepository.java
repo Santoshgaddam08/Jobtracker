1b67060c4dd2e171e39bb47bd4e70973dbf08c01
Santosh Kumar Gaddam <santoshgaddam08@gmail.com>
2026-01-15 12:56:01 -0500

    Add StatusHistoryRepository

---
+++ backend/src/main/java/com/santosh/jobtracker/applications/StatusHistoryRepository.java
@@ -0,0 +1,8 @@
+package com.santosh.jobtracker.applications;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface StatusHistoryRepository extends JpaRepository<StatusHistory, String> {
+    List<StatusHistory> findByUserIdAndApplicationIdOrderByChangedAtDesc(String userId, String applicationId);
+}
