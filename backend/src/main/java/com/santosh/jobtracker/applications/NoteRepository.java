c3201e0ad04821f9fb7869431cd3e1d48d6f78e4
Santosh Kumar Gaddam <santoshgaddam08@gmail.com>
2026-01-15 16:12:02 -0500

    Create NoteRepository.java

---
+++ backend/src/main/java/com/santosh/jobtracker/applications/NoteRepository.java
@@ -0,0 +1,8 @@
+package com.santosh.jobtracker.applications;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface NoteRepository extends JpaRepository<Note, String> {
+    List<Note> findByUserIdAndApplicationIdOrderByCreatedAtDesc(String userId, String applicationId);
+}
